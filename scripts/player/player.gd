806d0f342c8a1741e6e8e503c844fa3d9a7f0f09
Markos Nunes <105757405+MarkVN2@users.noreply.github.com>
2024-02-07 21:13:32 -0300

    [feat]: Character has limited jumps
    This is still strangely buggy since it sometimes stops out of nowhere.

--- scripts/player/player.gd
+++ scripts/player/player.gd
@@ -1,21 +1,24 @@
 class_name Player
 extends Actor
 
-const SPEED : int = 1500
-const JUMP_FORCE : int = 3000
+const SPEED:int = 1500
+const JUMP_FORCE:int = 3000
 
-var max_health : int = 100
-var health : int = max_health
+var max_health:int = 100
+var health:int = max_health
 
-var xp_to_level : int = 100
-var current_xp : int = 0
-var level : int = 1
+var level:int = 1
 
-var base_damage : int = 10
-var damage : int = base_damage 
+var base_damage:int = 10
+var damage:int = base_damage 
 
-var can_move : bool = true
-var is_jumping : bool = false
+var can_move:bool = true
+var is_jumping:bool = false
+
+@onready var sprite = $PSprite
+@onready var cast = $GroundCast
+
+var _player_velocity = Vector2.ZERO
 
 
 #func updateStats():
@@ -32,13 +35,27 @@ var is_jumping : bool = false
 #            max_health += item.health
 #            health += item.health
 
+
 func toggleMove():
 	can_move = false if can_move else true
 
-var _player_velocity = Vector2.ZERO
 
-func _physics_process(delta):
+func _physics_process(_delta):
 	if can_move == true:
 		var direction = Input.get_vector("move_left","move_right","disabled","disabled")
-		_player_velocity = direction * SPEED 
+		
+		if -1 < direction.x and direction.x < 1 and direction.x != 0:
+			sprite.flip_h = true
+		if  direction.x  <= -1:
+			sprite.flip_h = false
+			
+		_player_velocity = direction * SPEED  
 		apply_force(_player_velocity)
+		
+	if Input.is_action_pressed("jump") == true and is_jumping == false:
+		var jump_vector = Vector2(0,-1) * JUMP_FORCE
+		print(jump_vector)
+		apply_force(jump_vector)
+		is_jumping = true
+	if cast.is_colliding():
+		is_jumping = false
