f29d466e5f19769ccb96fa517650073ff00497aa
Markos Nunes <105757405+MarkVN2@users.noreply.github.com>
2024-02-10 06:53:04 -0300

    [visuals]: updated bob to bobv3
    better animations, not the best but not that horrid

--- scripts/player/player.gd
+++ scripts/player/player.gd
@@ -10,17 +10,17 @@ func _physics_process(delta):
 	if can_move == true:
 			if Input.is_action_pressed("move_left"):
 				velocity.x -= move_speed
-				sprite.flip_h = false
+				sprite.flip_h = true
 			if Input.is_action_pressed("move_right"):
 				velocity.x += move_speed
-				sprite.flip_h = true
+				sprite.flip_h = false
 			if Input.is_action_pressed("jump") and is_on_floor():
 				velocity.y = -jump_force
 			move_and_slide()
 			
 			if velocity.x != 0.0:
-				sprite.play_walk_animation()
+				sprite.play_walk_animation(move_speed)
 			else:
-				sprite.play_idle_animation()
+				sprite.play_idle_animation(move_speed)
 			if velocity.y != 0.0:
 				sprite.play_jump_animation()
